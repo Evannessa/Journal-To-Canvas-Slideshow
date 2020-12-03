d8424a80aedf35fbee38deb3a490e8e0e3b9f5e0
EvanesceExotica@gmail.com <EvaneseExotica@gmail.com>
2020-12-03 16:41:59 -0500

    Making a class

--- scripts/ClickImageInJournal.js
+++ scripts/ClickImageInJournal.js
@@ -3,18 +3,21 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 var displayScene;
 var displayTile;
 
-Hooks.on("ready", ()  =>{
+//Hooks.on("getSceneControlButtons", ClickImageInJournal.createSceneButton); //for scene control buttons on right
+Hooks.on("renderSidebarTab", createSceneButton); //for sidebar stuff on left
 
-	//if there isn't already a display scene
-	if (!DisplaySceneFound()){
-		//if there is no display scene or it's undefined
-		//generate a new one
-		GenerateDisplayScene();
-	}
-	else{
-		console.log("Display scene already exists :)");
-	}
-});
+// Hooks.on("ready", ()  =>{
+
+// 	//if there isn't already a display scene
+// 	if (!DisplaySceneFound()){
+// 		//if there is no display scene or it's undefined
+// 		//generate a new one
+// 		GenerateDisplayScene();
+// 	}
+// 	else{
+// 		console.log("Display scene already exists :)");
+// 	}
+// });
 
 Hooks.on("renderJournalSheet", (app, html, options) => {
 	html.find('img').attr("class", "clickableImage");
@@ -26,6 +29,8 @@ Hooks.on("renderJournalSheet", (app, html, options) => {
 	console.log("A journal sheet has opened");
 	//findClickableImage(html);
 });
+
+class ClickImageInJournal {
 
 function highlight(ev) {
 	ev.target.style.borderStyle = "solid"; //css("border-style", "solid", "border-color", "white", "border-width", "4px");
@@ -154,6 +159,38 @@ async function displayImage(ev) {
 	}
 }
 
+static createSceneButton(app, html){
+	if(!game.user.isGM){
+		return;
+	}
+	if(app.options.id == "scenes"){
+		let button = $("<button>Create Display Scene</button>");
+		button.click(buttonTest());//GenerateDisplayScene());
+		html.find(".directory-footer").prepend(button);
+	}
+
+
+}
+
+function buttonTest(){
+	console.log("DISPLAY BUTOTN CLICKED");
+}
+
+// static function getSceneControlButtons(buttons){
+// 	let notesButton = buttons.find(b => b.name === "token");
+
+// 	notesButton.tools.push({
+// 		name: "createDisplayScene",
+// 		title: game.i18n.localize("QE.CreateDisplayScene.Button"),
+// 		icon: "",
+// 		toggle: false,
+// 		button: true,
+// 		visible: game.user.isGM,
+// 		onClick: event => ClickImageInJournal.GenerateDisplayScene()
+
+// 	});
+// }
+
 async function GenerateDisplayScene() {
 	console.log("Generating new display scene");
 	//create a Display" scene 
@@ -173,13 +210,18 @@ async function GenerateDisplayScene() {
 			height: 2000,
 			backgroundColor: "#202020",
 			padding: 0});
+
+		const tex = await loadTexture("darkbackground.jpg");
+		var dimensionObject =  calculateAspectRatioFit(tex.width, tex.height, displayScene.data.width, displayScene.data.height);
 		displayTile = await Tile.create({
 			img: "darkbackground.jpg",
-			width: 2000,
-			height: 1000,
-			y: 0,
-			x: 1000//(displayScene.data.width/ 2) - (dimensionObject.width / 2)
+			width: dimensionObject.width,
+			height: dimensionObject.height,
+			x: 0,
+			y: (displayScene.data.width/ 2) - (dimensionObject.width / 2)
 		});
+		await displayTile.update({x: (displayScene.data.width/ 2) - (dimensionObject.width / 2)});
+
 	}
 
 	//let display = await Scene.create({name: "Display", width: 2000, height: 2000, backgroundColor: "#202020"});
@@ -210,4 +252,5 @@ function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
 		height: srcHeight * ratio
 	};
 
+}
 }
