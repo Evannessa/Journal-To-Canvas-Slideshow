1932ebefccf10da05d6eea92d42805f6488721ee
EvanesceExotica@gmail.com <EvaneseExotica@gmail.com>
2020-12-18 15:54:37 -0500

    Sharing version

--- scripts/ClickImageInJournal.js
+++ scripts/ClickImageInJournal.js
@@ -25,15 +25,7 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 			console.log("No display scene");
 			return;
 		}
-		// if(displayTile == null){
-		// 	displayTile = await Tile.create({
-		// 	img: "darkbackground.jpg",
-		// 	width: 2000,
-		// 	height: 1000,
-		// 	y: 0,
-		// 	x: 1000//(displayScene.data.width/ 2) - (dimensionObject.width / 2)
-		// 	});
-		// }
+		
 		let element = ev.currentTarget;
 		let url = element.getAttribute("src");
 
@@ -47,11 +39,11 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 		const tex = await loadTexture(data.src);
 
 		var dimensionObject = calculateAspectRatioFit(tex.width, tex.height, displayScene.data.width, displayScene.data.height);
-		//DisplaySceneFound();
+
 		tileData.width = dimensionObject.width;
 		tileData.height = dimensionObject.height;
 		var displayTile = displayScene.getEmbeddedCollection("Tile")[0]; //canvas.tiles.placeables[0];
-		console.log(displayTile);
+
 		var scaleDownFactor = 200;
 		dimensionObject.width -= scaleDownFactor;
 		dimensionObject.height -= scaleDownFactor;
@@ -88,7 +80,7 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 		if (app.options.id == "scenes") {
 			let button = $("<button>Create or Show Display Scene</button>");
 			//if the display scene already exists, open and activate it; if not, create a new one
-			button.click(GenerateDisplayScene); //GenerateDisplayScene());
+			button.click(GenerateDisplayScene); 
 			html.find(".directory-footer").prepend(button);
 		}
 
@@ -105,8 +97,7 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 		if (!DisplaySceneFound()) {
 			displayScene = null;
 			displayTile = null;
-			console.log("Display scene " + displayScene);
-			console.log("Generating new display scene");
+
 			displayScene = await Scene.create({
 				name: "Display",
 				width: 100,
@@ -114,9 +105,7 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 				backgroundColor: "#202020",
 				padding: 0
 			});
-			console.log(displayScene.data.width + " by " + displayScene.data.height);
 			await displayScene.activate();
-			console.log(canvas.scene);
 			await displayScene.update({
 				name: "Display",
 				width: 2000,
@@ -126,11 +115,9 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 			});
 
 			//create a tile for the scene
-			console.log("Display Scene Dimensions" + displayScene.data.width + " by " + displayScene.data.height);
 			const tex = await loadTexture("darkbackground.jpg");
 			var dimensionObject = calculateAspectRatioFit(tex.width, tex.height, displayScene.data.width, displayScene.data.height);
-			console.log("Texture dimensions " + tex.width + " by " + tex.height);
-			console.log("Dimension Object dimensions " + dimensionObject.width + " by " + dimensionObject.height);
+
 			displayTile = await Tile.create({
 				img: "darkbackground.jpg",
 				width: dimensionObject.width,
@@ -138,12 +125,11 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 				x: 0,
 				y: (displayScene.data.height / 2) - (dimensionObject.height / 2)
 			});
-			console.log(displayScene.data.width + " By " + displayScene.data.height)	;
+
 			canvas.draw();
 
 		}
 		else{
-			console.log("Display scene already exists");
 			//if the display scene exits already, just activate it
 			displayScene.activate();
 
@@ -184,8 +170,8 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 	function addImage(app, url, currentJournalId){
 		var journalEntry;
 		//create image tag with url of item
-		var containerParagraph;//document.createElement("p");
-		var image = new Image();//document.createElement("IMG");
+		var containerParagraph;
+		var image = new Image();
 		//append the child to the body of the journal entry -- gotta figure out how to add it to the journal entry specifically
 		var journalDiv = document.getElementById(currentJournalId);
 
@@ -193,12 +179,9 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 		var editorContent = journalForm.getElementsByClassName("editor-content")[0];
 		containerParagraph = journalForm.getElementsByClassName("editor-content")[0].querySelector("p");//appendChild(containerParagraph);
 		containerParagraph.appendChild(image);
-		//journalForm.getElementsByClassName("editor-content")[0].appendChild(image);
 		image.src = url;
-		//console.log(editorContent.innerHTML);
 		app.object.data.content = editorContent.innerHTML;
 		console.log(app.object.data.content);
-	//	let updated = await
 		 app.object.update({content : app.object.data.content});
 
 		 app.render(false, {});
@@ -207,7 +190,6 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 	}
 
 	function addImageToJournal(app, url){
-		//THIS does not work --- it resets when you refresh. Need to find a better way to access it.
 		app.object.data.content += "<img src=" + url + ">";
 	}
 
@@ -231,7 +213,6 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 			console.log("ELSE Response path is " + response.path)
 		}
 		addImage(app, response.path, currentJournalId);
-		//addImageToJournal(app, response.path);
 
 	}
 
@@ -239,47 +220,20 @@ console.log("Hello world! This code runs immediately when the file is loaded");
 //Hooks.on("getSceneControlButtons", ClickImageInJournal.createSceneButton); //for scene control buttons on right
 Hooks.on("renderSidebarTab", createSceneButton); //for sidebar stuff on left
 
-// Hooks.on("ready", ()  =>{
-
-// 	//if there isn't already a display scene
-// 	if (!DisplaySceneFound()){
-// 		//if there is no display scene or it's undefined
-// 		//generate a new one
-// 		GenerateDisplayScene();
-// 	}
-// 	else{
-// 		console.log("Display scene already exists :)");
-// 	}
-// });
-
 
 Hooks.on("renderJournalSheet", (app, html, options) => {
-	//addImageToJournal(app, "1920x1080.jpg");
-	console.log(app);
-	console.log(html);
 	var currentJournalId = html[0].id;
-	console.log(options);
 	html.find('img').attr("class", "clickableImage");
-	document.querySelector("form.editable").addEventListener("drop", (event) => {
-		console.log("Dropped something");
-		handleDrop(app, event, currentJournalId);
-		//_activateEditor()
-		//html.find('editor-content[data-edit]').each((i, div) => { app._activateEditor(div)});
-		//html.find('editor-content').each((i, div) => _activateEditor(div.get_attribute("data-edit")));
-		//console.log(app.object.data.content);
-		//app.object.update({content: app.object.data.content});
-	});
+	// document.querySelector("form.editable").addEventListener("drop", (event) => {
+	// 	handleDrop(app, event, currentJournalId);
+		
+	// });
 	html.find('.clickableImage').each((i, div) => {
 		div.addEventListener("click", /*findClickableImage*/ displayImage, false);
 		div.addEventListener("mouseover", highlight, false);
 		div.addEventListener("mouseout", dehighlight, false);
 	});
-	// document.getElementById("journal").addEventListener("drop", (event) => {
 
 
-	// }
-
-	//)
 	console.log("A journal sheet has opened");
-	//findClickableImage(html);
 });
